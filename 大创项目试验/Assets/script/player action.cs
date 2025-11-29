2d022745d3d3a2e707112c53ef988cfabf81b860
xinxiangheart <2258954295@qq.com>
2025-11-29 15:59:34 +0800

    111

--- 大创项目试验/Assets/script/player action.cs
+++ 大创项目试验/Assets/script/player action.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float playermovespeed;
+    public Rigidbody2D playerRB;
+    public Collider2D playerColl;
+    public Animator playAnim;
     void Start()
     {
-        
+        playerColl= GetComponent<Collider2D>();
+        playerRB= GetComponent<Rigidbody2D>();
+        playAnim= GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        playerMove();
+    }
+     
+    void playerMove()
+    {
+        float horizontalNum  =  Input.GetAxis("Horizontal");
+        float facenum = Input.GetAxisRaw("Horizontal");
+        playerRB.velocity = new Vector2(playermovespeed * horizontalNum,playerRB.velocity.y);
+        playAnim.SetFloat("run",Mathf.Abs( playermovespeed * horizontalNum));
+        if (facenum != 0)
+        {
+            transform.localScale = new Vector3(3*facenum, transform.localScale.y, transform.localScale.z);
+        }
     }
 }
